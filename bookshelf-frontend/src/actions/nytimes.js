6402c5cf05aa202b227f3857d6ae54ab2b793ebb
radville <laura.radville@gmail.com>
2019-12-11 15:49:56 -0500

    genre component redirects

--- bookshelf-frontend/src/actions/nytimes.js
+++ bookshelf-frontend/src/actions/nytimes.js
@@ -1,6 +1,6 @@
-export const FETCH_GENRE_BOOKS = "FETCH_GENRE_BOOKS"
+// export const FETCH_GENRE_BOOKS = "FETCH_GENRE_BOOKS"
 export const SET_GENRE_BOOKS = "SET_GENRE_BOOKS"
-export const FETCH_GENRES = "FETCH_GENRES"
+// export const FETCH_GENRES = "FETCH_GENRES"
 export const SET_GENRES = "SET_GENRES"
 
 // saves the NYTimes books in a genre to redux state
@@ -17,8 +17,8 @@ export const fetchGenreBooks = genre => {
             .then(response => response.json())
             .then(data => {
                 dispatch(setGenreBooks(data))
-            });
-            // .then(window.location.href= `/books/${slug}`)
+            })
+            .then(() => {window.location.href= `/books/${slug}`})
 }
 
 // sets genres on the page after they're fetched
