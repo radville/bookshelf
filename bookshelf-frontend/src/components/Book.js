38a02752d8d76648ba35ea31d538030d4651cf81
radville <laura.radville@gmail.com>
2019-12-12 14:42:03 -0500

    styled books container

--- bookshelf-frontend/src/components/Book.js
+++ bookshelf-frontend/src/components/Book.js
@@ -7,12 +7,10 @@ class Book extends Component {
     return(
       <div key={book.id} className="card bg-light mb-3">
         <li className="card-block">
-          <div class="card-header">
-            <a href={book.amazon_product_url} style={{color: "black", fontWeight: "bold", fontSize: "1.2em"}}>
+          <div class="card-body">
+            <a href={book.amazon_product_url} style={{ fontWeight: "bold", fontSize: "1.2em" }}>
               {book.title}
             </a>
-          </div>
-          <div class="card-body">
             <div className="card-title" style={{fontWeight: "bold"}}>by {book.author}</div>
             <img className="img-fluid" src={book.book_image} alt={book.title}/>
             <p className="card-text">{book.description}</p>
