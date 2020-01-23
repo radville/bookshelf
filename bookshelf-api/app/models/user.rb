d32ff3a4cbc05cbc478a08057d462feecb463f38
radville <laura.radville@gmail.com>
2020-01-23 12:56:28 -0500

    added user model and controller

--- bookshelf-api/app/models/user.rb
+++ bookshelf-api/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  has_many :userbooks
-  has_many :books, through: :userbooks
+    has_secure_password
+
+    has_many :userbooks
+    has_many :books, through: :userbooks
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 4 }
+    validates :email, presence: true
+    validates :email, uniqueness: truevalidates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- bookshelf-api/config/routes.rb
+++ bookshelf-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create, :show]
   resources :userbooks, only: [:new, :create, :show, :destroy]
   resources :books
 
