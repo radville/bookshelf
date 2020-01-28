df5c7d7237674d8dd36933767ab96428e28e4947
radville <laura.radville@gmail.com>
2020-01-28 15:12:03 -0500

    created database

--- bookshelf-api/app/models/user.rb
+++ bookshelf-api/app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
     validates :username, uniqueness: true
     validates :username, length: { minimum: 4 }
     validates :email, presence: true
-    validates :email, uniqueness: truevalidates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email, uniqueness: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
