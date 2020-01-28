28a1e23ff0fed726d7b3f568dd9664a44eef4891
radville <laura.radville@gmail.com>
2020-01-28 16:27:04 -0500

    added helper methods for user model

--- bookshelf-api/app/controllers/application_controller.rb
+++ bookshelf-api/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+    skip_before_action :verify_authenticity_token
+
+    helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
+
+    def login!
+        !!session[:user_id]
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def authorized_user?
+        @user == current_user
+    end
+
+    def logout!
+        session.clear
+    end
+
 end
