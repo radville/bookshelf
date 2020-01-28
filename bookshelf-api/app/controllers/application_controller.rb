b25f11fc82abe08830afab1f28254eeeb9666b81
radville <laura.radville@gmail.com>
2020-01-28 16:29:07 -0500

    added comments

--- bookshelf-api/app/controllers/application_controller.rb
+++ bookshelf-api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+    # prevent Rails from using its authenticity token because we're treating backend as API
     skip_before_action :verify_authenticity_token
 
     helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
