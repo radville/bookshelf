d9cd8a238717bef8859c5b7e3f274d1de244f2a7
radville <laura.radville@gmail.com>
2020-02-10 12:30:30 -0500

    added user login routes

--- bookshelf-api/config/routes.rb
+++ bookshelf-api/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :userbooks, only: [:new, :create, :show, :destroy]
   resources :books
 
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
   root to: 'books#index'
   get '/genres', to: 'bestsellers#genres'
   get '/bestsellers/:genre', to: 'bestsellers#show'
