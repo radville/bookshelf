1cf8712771e935aa676e4b7b9034ed12083f32de
radville <laura.radville@gmail.com>
2020-02-14 10:10:15 -0500

    added signup form

---
+++ bookshelf-frontend/src/components/registrations/Signup.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+        username: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+        errors: ''
+        };
+    }
+
+    handleChange = (event) => {
+        const {name, value} = event.target
+        this.setState({
+        [name]: value
+        })
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    };
+
+    render() {
+        const {username, email, password, password_confirmation} = this.state
+        return (
+            <div>
+                <h1>Sign Up</h1>
+                <form onSubmit={this.handleSubmit}>
+                <input
+                    placeholder="username"
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={this.handleChange}
+                />
+                <input
+                    placeholder="email"
+                    type="text"
+                    name="email"
+                    value={email}
+                    onChange={this.handleChange}
+                />
+                <input 
+                    placeholder="password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={this.handleChange}
+                />
+                <input
+                    placeholder="password confirmation"
+                    type="password"
+                    name="password_confirmation"
+                    value={password_confirmation}
+                    onChange={this.handleChange}
+                />
+                
+                <button placeholder="submit" type="submit">
+                    Sign Up
+                </button>
+            
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Signup;
