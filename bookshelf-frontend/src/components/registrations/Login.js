fa94984272d0a184a3e3f9136c38594467288ea0
radville <laura.radville@gmail.com>
2020-02-16 13:42:19 -0500

    login.js posts user info to backend

--- bookshelf-frontend/src/components/registrations/Login.js
+++ bookshelf-frontend/src/components/registrations/Login.js
@@ -23,43 +23,85 @@ class Login extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
+        const {username, email, password} = this.state
+
+        let user = {
+            username: username,
+            email: email,
+            password: password
+        }
+
+        axios.post('http://localhost:3001/login', {user}, {withCredentials: true})
+        .then(response => {
+            if (response.data.logged_in) {
+                this.props.handleLogin(response.data)
+                this.redirect()
+            } else {
+                this.setState({
+                    errors: response.data.errors
+                })
+            }
+        })
+        .catch(error => console.log('api errors:', error))
     };
+
+    redirect = () => {
+        this.props.history.push('/')
+    }
+
+    handleErrors = () => {
+        return (
+            <div>
+                <ul>
+                    {this.state.errors.map(error => {
+                        return <li key={error}>{error}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
 
     render() {
         const {username, email, password} = this.state
+
         return (
             <div>
                 <h1>Log In</h1>
                 <form onSubmit={this.handleSubmit}>
-                <input
-                    placeholder="username"
-                    type="text"
-                    name="username"
-                    value={username}
-                    onChange={this.handleChange}
-                />
-                <input
-                    placeholder="email"
-                    type="text"
-                    name="email"
-                    value={email}
-                    onChange={this.handleChange}
-                />
-                <input
-                    placeholder="password"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={this.handleChange}
-                />
-                <button placeholder="submit" type="submit">
-                    Log In
-                </button>
-                <div>
-                    or <Link to='/signup'>sign up</Link>
-                </div>
-                
+                    <input
+                        placeholder="username"
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        placeholder="email"
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleChange}
+                    />
+                    <button placeholder="submit" type="submit">
+                        Log In
+                    </button>
+                    <div>
+                        or <Link to='/signup'>sign up</Link>
+                    </div>
                 </form>
+
+                <div>
+                    {
+                        this.state.errors ? this.handleErrors() : null
+                    }
+                </div>
             </div>
         );
     }
