60f8a339b5dcea16abef23f95d04d185ca888d25
radville <laura.radville@gmail.com>
2020-02-17 12:58:10 -0500

    signup.js posts user info to backend

--- bookshelf-frontend/src/components/registrations/Signup.js
+++ bookshelf-frontend/src/components/registrations/Signup.js
@@ -24,7 +24,43 @@ class Signup extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-    };
+        const {username, email, password, password_confirmation} = this.state
+    
+        let user = {
+        username: username,
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation
+        }
+
+        axios.post('http://localhost:3001/users', {user}, {withCredentials: true})
+            .then(response => {
+                if (response.data.status === 'created') {
+                    this.props.handleLogin(response.data)
+                    this.redirect()
+                } else {
+                    this.setState({
+                    errors: response.data.errors
+                    })
+                }
+            })
+            .catch(error => console.log('api errors:', error))
+        };
+
+    redirect = () => {
+        this.props.history.push('/')
+    }
+
+    handleErrors = () => {
+        return (
+        <div>
+            <ul>{this.state.errors.map((error) => {
+                return <li>key={error}>{error}</li>
+            })}
+            </ul> 
+        </div>
+        )
+    }
 
     render() {
         const {username, email, password, password_confirmation} = this.state
@@ -32,40 +68,45 @@ class Signup extends Component {
             <div>
                 <h1>Sign Up</h1>
                 <form onSubmit={this.handleSubmit}>
-                <input
-                    placeholder="username"
-                    type="text"
-                    name="username"
-                    value={username}
-                    onChange={this.handleChange}
-                />
-                <input
-                    placeholder="email"
-                    type="text"
-                    name="email"
-                    value={email}
-                    onChange={this.handleChange}
-                />
-                <input 
-                    placeholder="password"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={this.handleChange}
-                />
-                <input
-                    placeholder="password confirmation"
-                    type="password"
-                    name="password_confirmation"
-                    value={password_confirmation}
-                    onChange={this.handleChange}
-                />
-                
-                <button placeholder="submit" type="submit">
-                    Sign Up
-                </button>
+                    <input
+                        placeholder="username"
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        placeholder="email"
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        placeholder="password confirmation"
+                        type="password"
+                        name="password_confirmation"
+                        value={password_confirmation}
+                        onChange={this.handleChange}
+                    />
+                    
+                    <button placeholder="submit" type="submit">
+                        Sign Up
+                    </button>
             
                 </form>
+                <div>
+                    {
+                        this.state.errors ? this.handleErrors() : null
+                    }
+                </div>
             </div>
         );
     }
