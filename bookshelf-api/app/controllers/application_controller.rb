939c855debbdb0e4f45f672b65c69af49236c839
radville <laura.radville@gmail.com>
2020-02-19 15:35:46 -0500

    added logged_in helper method

--- bookshelf-api/app/controllers/application_controller.rb
+++ bookshelf-api/app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
         !!session[:user_id]
     end
 
+    def logged_in?
+        !!session[:user_id]
+    end
+
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
