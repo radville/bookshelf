39298c66c430f023bc95939ce62559ab36de8038
radville <laura.radville@gmail.com>
2020-03-04 10:53:58 -0500

    bundle update

--- bookshelf-api/app/controllers/sessions_controller.rb
+++ bookshelf-api/app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         
         if @user && @user.authenticate(session_params[:password])
             login!
-            binding.pry
+            # binding.pry
             render json: {
                 logged_in: true,
                 user: @user
